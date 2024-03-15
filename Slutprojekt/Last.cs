cc63fcf621a4a4425ae58cd2234b512be880af4c
EdvinGitHub <Edvin.arvesjo@elev.ga.ntig.se>
2024-03-15 12:42:58 +0100

    asd

--- Slutprojekt/Last.cs
+++ Slutprojekt/Last.cs
@@ -1,11 +1,14 @@
 ﻿namespace Slutprojekt;
+
+using System.Numerics;
 using Raylib_cs;
 public class Last : Player
 {
-    bool groundTrue = true;
+    bool groundTrue = false;
     float gravity = -10f;
     List<Rectangle> walls = new List<Rectangle>();
-    Rectangle wall1 = new Rectangle(0, 680 ,750, 20);
+    Rectangle wall1 = new Rectangle(0, 750 ,750, 50);
+    Rectangle test = new Rectangle(0, 750, 0, 0);
     public override void Update(float deltaTime)
     {
         walls.Add(wall1);
@@ -14,14 +17,21 @@ public class Last : Player
         for (int i = 0; i < walls.Count; i++)
         {
             if(Raylib.CheckCollisionRecs(ground, walls[i]))
-            {groundTrue = true;}
-            else
-            {groundTrue = false;}
-
+            {groundTrue = true;
+            test.Position = walls[i].Position; 
+            ground.Y = test.Y-100;}
+            // else
+            // {groundTrue = false;}
 
         }
-        if(groundTrue = false)
-        { hej.Y -= gravity;}
+        if(groundTrue == false)
+        {hej.Y -= gravity;}
+        if (groundTrue == true)
+        {
+           
+            hej.Y = ground.Y - 100;
+
+        }
        
         
     }
