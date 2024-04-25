0249a57ca3593305bfa4c8b6fb19d097ffc4aa58
EdvinGitHub <Edvin.arvesjo@elev.ga.ntig.se>
2024-04-26 00:02:53 +0200

    fixade ett stort problem:)
    (jk)

--- Slutprojekt/Player.cs
+++ Slutprojekt/Player.cs
@@ -71,7 +71,7 @@ public class PlayerSkript : Entity
     }
       
 
-  }
+    }
         if (groundTrue == true)
         {
                 if(playerCharater.Y >= groundCheck.Y - playerCharater.Height)
